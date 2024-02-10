99f6eb999ea3c8c75e662fd7df967c21db30210e
Tessyrich <olyrich3@gmail.com>
2024-02-10 09:44:11 +0100

    adjust git

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist", // Make sure this matches the directory you specified in the deployment script
+  },
+});
